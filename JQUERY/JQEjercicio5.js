b00706fe7a2e8591b325a2d2821d0b955ca73532
Jhonny0102 <fjhon868@gmail.com>
2023-10-04 11:09:27 +0200

    nuevo

--- JQUERY/JQEjercicio5.js
+++ JQUERY/JQEjercicio5.js
@@ -3,11 +3,14 @@ $(document).ready(function () {
     for( var i = 1 ; i < 10 ; i++){
         //Guardamos en la variable nuemeros aleatorios entre 1 y 100.
         let aleatorio = parseInt(Math.random() * 100) + 1;
+        //Creamos un objeto button con sus propiedades.
         var boton = $("<button>",{
             "text": aleatorio,
             "value": aleatorio
         });
+        //Creamos dentro del padre los objetos.
         $("#botones").append(boton);
+        //Detecta si dan un click en un boton.
         boton.click(function() { 
             var numero = parseInt($(this).val()); 
             var suma = parseInt($("#resultado").text()); 
