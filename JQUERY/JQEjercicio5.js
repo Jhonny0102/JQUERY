b509d72572b6beb2d40be453e39f7184bed66a47
Jhonny0102 <fjhon868@gmail.com>
2023-10-04 10:51:38 +0200

    commit comentarios

--- JQUERY/JQEjercicio5.js
+++ JQUERY/JQEjercicio5.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     //Bucle de 10 vueltas para hacer lo siguiente
     for( var i = 1 ; i < 10 ; i++){
+        //Guardamos en la variable nuemeros aleatorios entre 1 y 100.
         let aleatorio = parseInt(Math.random() * 100) + 1;
         var boton = $("<button>",{
             "text": aleatorio,
